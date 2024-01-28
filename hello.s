558d0c5ba263a10180629ca20ec0c2f14eb0e9b4
visionthex <sandseclos@gmail.com>
2024-01-27 23:03:22 -0500

    Updating my hello file

--- hello.s
+++ hello.s
@@ -1,24 +1,19 @@
-# 0 "hello.S"
-# 0 "<built-in>"
-# 0 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 0 "<command-line>" 2
-# 1 "hello.S"
 .intel_syntax noprefix
 .section .data
-  hello: .asciz "Hello, World!"
+    hello: .asciz "Hello, World!"
 
 .section .text
 .global _start
 
 _start:
-  ; Your code here to print "Hello, World!"
-  ; Exit syscall
-  mov eax, 1 ; syscall number for Exit
-  xor ebx, ebx ; exit code 0
-  int 0x80 ; call kernel
+    # Write syscall
+    mov eax, 4 # syscall number for write
+    mov ebx, 1 # file descriptor (stdout)
+    lea ecx, [hello] # pointer to the message
+    mov edx, 13 # length of the message
+    int 0x80 # call kernel
 
-  ; Exit syscall
-  mov eax, 1 ; syscall number for exit
-  xor ebx, ebx ; exit code 0
-  int 0x80 ; call
+    # Exit syscall
+    mov eax, 1 # syscall number for exit
+    xor ebx, ebx # exit code 0
+    int 0x80 # call kernel
